8dc8a1e2f7405aac3ba2ee432fcb53037e4bced0
Wellington Santos <dev.wsantos@outlook.com>
2025-07-02 23:03:04 -0300

    feat(api): implementação da interface do repository (IContatoRepository)

---
+++ backend/API/Repositories/IContatoRepository.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public interface IContatoRepository
+    {
+        Task<IEnumerable<Contato>> GetAllAsync();
+        Task<Contato?> GetByIdAsync(Guid id);
+        Task<Contato?> GetByEmailAsync(string email);
+        Task AddAsync(Contato contato);
+        Task UpdateAsync(Contato contato);
+        Task DeleteAsync(Contato contato);
+    }
+}

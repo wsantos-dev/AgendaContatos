8ff0573bbde6b645957f96d8dd0bf356c99fdfe6
Wellington Santos <dev.wsantos@outlook.com>
2025-07-02 22:18:53 -0300

    Add project files.

---
+++ backend/API/Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Contato
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
+    }
+}

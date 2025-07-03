2254566641df0dabccacd532df7b3ee823fc3ef3
Wellington Santos <dev.wsantos@outlook.com>
2025-07-02 23:32:59 -0300

    feat(api): Criação da interface do Service e a implementação do Service

---
+++ backend/API/Services/ContatoService.cs
@@ -0,0 +1,63 @@
+﻿using API.DTOs;
+using API.Models;
+using API.Repositories;
+using AutoMapper;
+
+namespace API.Services
+{
+    public class ContatoService : IContatoService
+    {
+        private readonly IContatoRepository _repo;
+        private readonly IMapper _mapper;
+
+        public ContatoService(IContatoRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ContatoReadDTO>> GetAllAsync()
+        {
+            var contatos = await _repo.GetAllAsync();
+            return _mapper.Map<IEnumerable<ContatoReadDTO>>(contatos);
+        }
+
+        public Task<ContatoReadDTO?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ContatoReadDTO> CreateAsync(ContatoCreateDTO dto)
+        {
+            if (await _repo.GetByEmailAsync(dto.Email) != null)
+                throw new Exception("Contato com este e-mail já existe.");
+
+            var contato = _mapper.Map<Contato>(dto);
+            contato.Id = Guid.NewGuid();
+            await _repo.AddAsync(contato);
+            return _mapper.Map<ContatoReadDTO>(contato);
+        }
+        public async Task UpdateAsync(Guid id, ContatoCreateDTO dto)
+        {
+            var contato = await _repo.GetByIdAsync(id);
+            if (contato == null)
+                throw new Exception("Contato não encontrado.");
+
+            contato.Nome = dto.Nome;
+            contato.Email = dto.Email;
+            contato.Telefone = dto.Telefone;
+
+            await _repo.UpdateAsync(contato);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var contato = await _repo.GetByIdAsync(id);
+            if (contato == null)
+                throw new Exception("Contato não encontrado.");
+
+            await _repo.DeleteAsync(contato);
+        }
+
+
+    }
+}
---
+++ backend/API/Services/IContatoService.cs
@@ -0,0 +1,13 @@
+﻿using API.DTOs;
+
+namespace API.Services
+{
+    public interface IContatoService
+    {
+        Task<IEnumerable<ContatoReadDTO>> GetAllAsync();
+        Task<ContatoReadDTO?> GetByIdAsync(Guid id);
+        Task<ContatoReadDTO> CreateAsync(ContatoCreateDTO dto);
+        Task UpdateAsync(Guid id, ContatoCreateDTO dto);
+        Task DeleteAsync(Guid id);
+    }
+}

4f137a785dbe0e8976b95097c9b9bff36cb3560d
Wellington Santos <dev.wsantos@outlook.com>
2025-07-02 23:04:59 -0300

    feat(api): Implementação base da classe do repository

---
+++ backend/API/Repositories/ContatoRepository.cs
@@ -0,0 +1,37 @@
+﻿using API.Models;
+
+namespace API.Repositories
+{
+    public class ContatoRepository : IContatoRepository
+    {
+        public Task AddAsync(Contato contato)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Contato contato)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Contato>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contato?> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contato?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Contato contato)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
